4dc8f3df8a70854226b5e345764efe0f58460945
caramelsauce <49214184+caramelsauce@users.noreply.github.com>
2019-09-22 20:27:11 -0300

    Add files via upload

--- fila.h
+++ fila.h
@@ -14,14 +14,20 @@ struct queue{
     struct elemento *inicio, *fim;
 };
 
+//seta o inicio e o fim da fila pra NULL
 void inicFila(struct queue *q);
 
+//retorna 1 se o inicio da fila for NULL ou 0 caso contrario
 int filaVazia(struct queue *q);
 
+//poe um novo nome no final da fila
 void enqueue(struct queue *q, char *nome);
 
+/*retorna uma 'char*' com o nome do primeiro da fila
+ *esse ponteiro deve ser desalocado */
 char *dequeue(struct queue *q);
 
+//printa todos os elementos da fila
 void printQueue(struct queue *q);
 
 #endif
--- filaPrioridade.h
+++ filaPrioridade.h
@@ -9,16 +9,23 @@ struct prioQueue{
     struct queue *qComum;
 };
 
+//aloca e inicializa as 3 filas contidas na fila de prioridade
 void inicFilaPrio(struct prioQueue *q);
 
+/*retorna 1 se a fila de prioridade estiver vazia ou 
+ 0 se pelo menos umas das subfilas conter um elemento*/
 int filaPrioVazia(struct prioQueue *q);
 
+//insere o nome em umas das fila de acordo com seu tipo(0-Idoso, 1-Especial, 2-Comum)
 void enqueuePrio(struct prioQueue *q, char *nome, int type);
 
+//remove um nome da fila respeitando a ordem das prioridades
 char *dequeuePrio(struct prioQueue *q);
 
+//printa as 3 filas
 void printQueuePrio(struct prioQueue *q);
 
+//conta os clientes chamados e implementa a politica de "aging"
 void aging(struct prioQueue *q, int op);
 
 #endif
